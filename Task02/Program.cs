ad1aafdb7816d97880390519f4480c7066d5df6e
Olesya K <olesjakraineva@gmail.com>
2023-12-22 18:59:29 +0300

    Added files

---
+++ Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int Ackermann(int N, int M)
+{
+    if (N < 0 || M < 0) throw new ArgumentException("Функция только для положительных чисел!");
+    if (N == 0) return M + 1;
+    if (M == 0) return Ackermann(N - 1, 1);
+    return Ackermann(N - 1, Ackermann(N, M - 1));
+}
+Console.WriteLine($"Функция Аккермана для чисел: {N} и {M} = {Ackermann(N, M)}");
---
+++ Task03/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(51);
+}
+Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+
+
+void ReversedRecursion(int[] arr, int start = 0)
+{
+    if (start != arr.Length)
+    {
+        ReversedRecursion(arr, start + 1);
+        Console.Write($"{array[start]}  ");
+    }
+}
+Console.Write("Рекурсия массива:  ");
+ReversedRecursion(array);
